886be60c96e79a40c117eb77733785a521a0db64
JapiKredi <jasper.bongers@yahoo.com>
2024-05-19 21:21:37 +0800

    Update code

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup, find_packages
 from typing import List
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
-   
+HYPEN_E_DOT='-e .'
+
+def get_requirement(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+print(get_requirement("./requirements.txt"))
+
+
+"""with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()"""     
+       
 
 __version__ = "0.0.4"
 REPO_NAME = "MongoDB_VectorDB"
